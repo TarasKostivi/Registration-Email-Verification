19b931974c4e26c2f829e18e2d98504d21dfa24b
TarasKostivi <taraskostsv@gmail.com>
2021-09-14 11:17:34 +0300

    Registration service

--- src/main/java/com/example/demo/registration/EmailValidator.java
+++ src/main/java/com/example/demo/registration/EmailValidator.java
@@ -1,7 +1,11 @@
 package com.example.demo.registration;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 import java.util.function.Predicate;
 
+@Component
 public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String s) {
